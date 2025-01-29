6f9ab6f404395674c5c2296626ca1a9d256b55b0
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-29 15:47:43 +0530

    k8s expense tf aws eks

---
+++ 20-bastion/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.58.0"
+    }
+  }
+backend "s3" {
+  bucket = "terraform-aws-eks-remote-state"
+  key = "terraform-aws-eks-bastion"
+  region = "us-east-1"
+  dynamodb_table = "terraform-aws-eks-remote-state-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
