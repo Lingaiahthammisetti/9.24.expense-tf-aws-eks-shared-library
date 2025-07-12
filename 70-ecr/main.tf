443b25d5f4a6817d07a48164b703bd9b0e3b5535
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-12 10:18:46 +0530

    k8s expense aws eks shared library

--- 70-ecr/main.tf
+++ 70-ecr/main.tf
@@ -8,6 +8,7 @@ resource "aws_ecr_repository" "backend" {
     image_scanning_configuration {
         scan_on_push = true
     }
+    force_delete = true
 }
 
 resource "aws_ecr_repository" "frontend" {
@@ -17,4 +18,5 @@ resource "aws_ecr_repository" "frontend" {
     image_scanning_configuration {
         scan_on_push = true
     }
+    force_delete = true
 }
